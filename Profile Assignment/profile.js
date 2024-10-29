2d4571c3e846ec910599824772a02d92b314b3db
EthanPurdue <etbusha@purdue.edu>
2024-10-29 11:55:04 -0400

    Delete Profile Assignment directory

--- Profile Assignment/profile.js
+++
@@ -1,7 +0,0 @@
-const setup = () => {
-    $("#name").text("Ethan Busha");
-    $("#major").text("CIT");
-    $("#year").text("2025");
-    $("h4").html("<em>United States</em>");
-}
-$(document).ready(setup)
