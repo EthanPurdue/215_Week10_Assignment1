77aa7ead283f7f79a4b872c68c4c755ae7a5101f
EthanPurdue <etbusha@purdue.edu>
2024-10-29 11:55:33 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,7 @@
+const setup = () => {
+    $("#name").text("Ethan Busha");
+    $("#major").text("CIT");
+    $("#year").text("2025");
+    $("h4").html("<em>United States</em>");
+}
+$(document).ready(setup)
